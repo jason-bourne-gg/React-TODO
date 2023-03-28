042f876f049fe8661ec7349d558c4f5ae8e5d2e3
jason-bourne-gg <car10@iitbbs.ac.in>
2023-03-29 00:54:43 +0530

    most of the features

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,51 @@
+import React, {useState, useRef, useEffect} from "react";
 import TodoList from "./TodoList";
+// import uuidv4 from 'uuid/v4';
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todo-storage'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect (() => {
+    const storedtodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedtodos) setTodos(storedtodos)
+  },[])
+
+  useEffect (() =>{
+    localStorage.setItem (LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggletodos(id) {
+    const newtodos = [...todos]
+    const todo = newtodos.find(todos =>todo.id === id)
+    todo.completed = !todo.completed
+    setTodos(newtodos)
+  }
+
+  function handleAddTodo (e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    //console.log(name)
+    setTodos (oldtodos => {
+      return ([...oldtodos, {id:uuidv4(), name:name, completed: false}])
+    })
+    todoNameRef.current.value = null
+
+  }
+
   return (
     //this is JSX (javascript XML); react's version of HTML
-    <TodoList/>
+    <>
+    {/*above syntax is shorthand of <fragment></fragment>  */}
+      <TodoList todos = {todos} toggletodos={toggletodos}/>
+      <input ref= {todoNameRef} type="text"></input>
+      <button onClick={handleAddTodo}> ADD TODO </button> 
+      <button> Clear TODOs </button> 
+      <div>0 left ToDo</div>
+    </>
   );
 }
 
--- todo/src/TodoList.js
+++ todo/src/TodoList.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Todo from './Todo' 
 
-export default function TodoList() {
+export default function TodoList({todos, toggletodos}) {
   return (
-    <div>TodoList</div>
+   todos.map((todo) => {
+    return <Todo key ={todo.id} toggletodos={toggletodos} todo = {todo}/>
+   })
   )
 }
